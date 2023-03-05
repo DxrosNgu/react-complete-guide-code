ab314477f498149071c13409cbb4a7e8f19cc179
DxrosNgu <diego.267.ri@gmail.com>
2023-03-05 18:40:07 -0500

    some changes

--- code/07-sending-a-post-request/src/App.js
+++ code/07-sending-a-post-request/src/App.js
@@ -13,7 +13,7 @@ function App() {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch('https://react-http-6b4a6.firebaseio.com/movies.json');
+      const response = await fetch('https://enhanced-watch-374406-default-rtdb.firebaseio.com/movies.json');
       if (!response.ok) {
         throw new Error('Something went wrong!');
       }
@@ -43,7 +43,8 @@ function App() {
   }, [fetchMoviesHandler]);
 
   async function addMovieHandler(movie) {
-    const response = await fetch('https://react-http-6b4a6.firebaseio.com/movies.json', {
+    console.log(movie);
+    const response = await fetch('https://enhanced-watch-374406-default-rtdb.firebaseio.com/movies.json', {
       method: 'POST',
       body: JSON.stringify(movie),
       headers: {
@@ -52,6 +53,7 @@ function App() {
     });
     const data = await response.json();
     console.log(data);
+
   }
 
   let content = <p>Found no movies.</p>;
